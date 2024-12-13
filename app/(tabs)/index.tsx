d5e8fc6981c9b646c59250e9ed2390c63ae625b6
Cauê Moreira <162379957+Caue06@users.noreply.github.com>
2024-12-13 12:59:29 -0300

    Add files via upload

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View,Text } from "react-native";
+
+export default function YourApp(){
+  return(
+    <View style={{
+      flex:1,
+      justifyContent:'center',
+      alignItems:'center',
+    }}>
+      <Text>Aula de ADS 👨‍💻</Text>
+    </View>
+  )
+}
